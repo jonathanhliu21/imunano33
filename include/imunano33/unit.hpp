249aac317ff91d291430aeaf9859f9e8c5062dde
Jonathan Liu <jonathanhliu21@gmail.com>
2023-06-11 22:52:02 -0700

    add units

---
+++ include/imunano33/unit.hpp
@@ -0,0 +1,31 @@
+/**
+ * @file
+ * @brief Contains TempUnit and PressureUnit enums
+ */
+
+#ifndef INCLUDE_IMUNANO33_UNIT_HPP_
+#define INCLUDE_IMUNANO33_UNIT_HPP_
+
+namespace imunano33 {
+/**
+ * @brief An enumerator describing units of temperature values
+ */
+enum TempUnit {
+  FAHRENHEIT, //!< Converts temperature to Fahrenheit
+  CELSIUS,    //!< Converts temperature to Celsius
+  KELVIN      //!< Converts temperature to Kelvin
+};
+
+/**
+ * @brief An enumerator describing units of pressure values
+ */
+enum PressureUnit {
+  PASCAL, //!< Converts pressure to pascal
+  ATM,  //!< Converts pressure to standard atmospheric pressure, the pressure at
+        //!< sea level and at 0°C
+  MMHG, //!< Converts pressure to millimeters of mercury
+  PSI   //!< Converts pressure to pounds per square inch
+};
+} // namespace imunano33
+
+#endif
